09283f5334a106784dd14166be7767805d0f38b8
oguzuzdil <75681078+oguzuzdil@users.noreply.github.com>
2021-03-12 14:02:43 +0300

    Add files via upload

---
+++ untitled3.m
@@ -0,0 +1,9 @@
+clear all
+clc
+a = input('Adinizi giriniz', 's');
+x = input('1. notunuzu giriniz');
+y = input('2. notunuzu giriniz');
+z = (x+y)/2;
+disp(['Merhaba' ,(a) , num2str(x), 've', num2str(y), 'notlarinin ortalamasi =' ,num2str(z)]);
+
+
