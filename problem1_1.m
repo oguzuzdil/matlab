f62e99a0fa26e80f883e32ab51f235c5c482cc3a
oguzuzdil <75681078+oguzuzdil@users.noreply.github.com>
2021-03-12 14:01:33 +0300

    Add files via upload

---
+++ problem1_1.m
@@ -0,0 +1,6 @@
+%Ýkinci dereceden 2x^2-x+3=0 denkleminin çözümü?
+a=2, b=-1, c=3;
+x1 = -b/(2*a)+1/(2*a)*sqrt(b^2-4*a*c);
+disp('birinci kok degeri') , disp(x1);
+x2=-b/(2*a)-1/(2*a)*sqrt(b^2-4*a*c);
+disp('birinci kok degeri') , disp(x1);
